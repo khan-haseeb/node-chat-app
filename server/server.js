8a6efd51485b39f8b93f16b64f802c27b5486863
Your Name <haseebk521@gmail.com>
2018-11-14 10:44:17 -0800

    chat server among clients

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express=require('express');
 const http=require('http');
-const socketIO=require('socket.iO');
+const socketIO = require('socket.io');
 
 const publicpath=path.join(__dirname, '../public' );
 const port=process.env.PORT || 3000;
