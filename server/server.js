5fe8edaf2fa235ebe6d2829220b3e72f9baa37b5
Your Name <haseebk521@gmail.com>
2018-11-14 07:35:43 -0800

    add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.iO');
 
 const publicpath=path.join(__dirname, '../public' );
-const port=process.env.PORT || 300
+const port=process.env.PORT || 3000
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
+io.on('connection',(socket) => {
+	console.log('new user connected');
+
+	socket.on('disconnect', () => {
+		console.log('disconnected form client');
+	});
+});
+
+
 
 app.use(express.static(publicpath));
 
-app.listen(port,() => {
+server.listen(port,() => {
 	console.log(`server started at port ${port}`);
 });
